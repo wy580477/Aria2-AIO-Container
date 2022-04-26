3d633121acc9de44e88aef681f5e8996db93e3b8
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-04-26 16:01:14 +0800

    Update ytdlptorclone.sh

--- content/ytdlptorclone.sh
+++ content/ytdlptorclone.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 
-DRIVE_NAME_AUTO="$(sed -n '1p' /mnt/config/rclone.conf | sed "s/\[//g" | sed "s/\]//g")"
-if [ "${DRIVE_NAME}" = "auto" ]; then
-    DRIVENAME=${DRIVE_NAME_AUTO}
-else
-    DRIVENAME=${DRIVE_NAME}
-fi
+DATE_TIME() {
+    date +"%m/%d %H:%M:%S"
+}
 
+DRIVE_NAME"$(grep ^drive-name /mnt/config/aria2/script.conf | cut -d= -f2-)"
 DRIVE_DIR="$(grep ^drive-dir /mnt/config/aria2/script.conf | cut -d= -f2-)"
+REMOTE_PATH="${DRIVE_NAME}:${DRIVE_DIR}"
 FILEPATH=$(echo $1 | sed 's:[^/]*$::')
 FILENAME=$(echo $1 | sed 's:/.*/::')
 
 if [[ "${POST_MODE}" =~ "move" ]]; then
-    curl -s -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"srcFs":"'"${FILEPATH}"'","srcRemote":"'"${FILENAME}"'","dstFs":"'"${DRIVENAME}"'\:'"${DRIVE_DIR}"'","dstRemote":"'"${FILENAME}"'","_async":"true"}' ''${RCLONE_ADDR}'/operations/movefile'
+    curl -s -S -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"srcFs":"'"${FILEPATH}"'","srcRemote":"'"${FILENAME}"'","dstFs":"'"${REMOTE_PATH}"'","dstRemote":"'"${FILENAME}"'","_async":"true"}' ''${RCLONE_ADDR}'/operations/movefile'
+    EXIT_CODE=$?
+    if [ ${EXIT_CODE} -eq 0 ]; then
+        echo "$(DATE_TIME) [INFO] Successfully send job to rclone: $1 -> ${REMOTE_PATH}"
+    else
+        echo "$(DATE_TIME) [ERROR] Failed to send job to rclone: $1"
+    fi
 elif [[ "${POST_MODE}" =~ "copy" ]]; then
-    curl -s -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"srcFs":"'"${FILEPATH}"'","srcRemote":"'"${FILENAME}"'","dstFs":"'"${DRIVENAME}"'\:'"${DRIVE_DIR}"'","dstRemote":"'"${FILENAME}"'","_async":"true"}' ''${RCLONE_ADDR}'/operations/copyfile'
+    curl -s -S -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"srcFs":"'"${FILEPATH}"'","srcRemote":"'"${FILENAME}"'","dstFs":"'"${REMOTE_PATH}"'","dstRemote":"'"${FILENAME}"'","_async":"true"}' ''${RCLONE_ADDR}'/operations/copyfile'
+    EXIT_CODE=$?
+    if [ ${EXIT_CODE} -eq 0 ]; then
+        echo "$(DATE_TIME) [INFO] Successfully send job to rclone: $1 -> ${REMOTE_PATH}"
+    else
+        echo "$(DATE_TIME) [ERROR] Failed to send job to rclone: $1"
+    fi
 fi
