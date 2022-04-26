3919145802d5c5839f6d7ef2ba76f39aa30d1645
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-04-26 11:29:56 +0800

    Update ytdlptorclone.sh

--- content/ytdlptorclone.sh
+++ content/ytdlptorclone.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-DRIVE_NAME_AUTO="$(sed -n '1p' /mnt/data/config/rclone.conf | sed "s/\[//g" | sed "s/\]//g")"
+DRIVE_NAME_AUTO="$(sed -n '1p' /mnt/config/rclone.conf | sed "s/\[//g" | sed "s/\]//g")"
 if [ "${DRIVE_NAME}" = "auto" ]; then
     DRIVENAME=${DRIVE_NAME_AUTO}
 else
     DRIVENAME=${DRIVE_NAME}
 fi
 
-DRIVE_DIR="$(grep ^drive-dir /mnt/data/config/script.conf | cut -d= -f2-)"
+DRIVE_DIR="$(grep ^drive-dir /mnt/config/aria2/script.conf | cut -d= -f2-)"
 FILEPATH=$(echo $1 | sed 's:[^/]*$::')
 FILENAME=$(echo $1 | sed 's:/.*/::')
 
 if [[ "${POST_MODE}" =~ "move" ]]; then
-    rclone rc --user "${USER}" --pass "${PASSWORD}" --rc-addr=localhost:56802 operations/movefile srcFs="${FILEPATH}" srcRemote="${FILENAME}" dstFs="${DRIVENAME}":"${DRIVE_DIR}" dstRemote="${FILENAME}" _async=true
+    curl -s -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"srcFs":"'"${FILEPATH}"'","srcRemote":"'"${FILENAME}"'","dstFs":"'"${DRIVENAME}"':/'"${DRIVE_DIR}"'","dstRemote":"'"${FILENAME}"'","_async":"true"}' ''${RCLONE_ADDR}'/operations/movefile'
 elif [[ "${POST_MODE}" =~ "copy" ]]; then
-    rclone rc --user "${USER}" --pass "${PASSWORD}" --rc-addr=localhost:56802 operations/copyfile srcFs="${FILEPATH}" srcRemote="${FILENAME}" dstFs="${DRIVENAME}":"${DRIVE_DIR}" dstRemote="${FILENAME}" _async=true
+    curl -s -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"srcFs":"'"${FILEPATH}"'","srcRemote":"'"${FILENAME}"'","dstFs":"'"${DRIVENAME}"':/'"${DRIVE_DIR}"'","dstRemote":"'"${FILENAME}"'","_async":"true"}' ''${RCLONE_ADDR}'/operations/copyfile'
 fi
