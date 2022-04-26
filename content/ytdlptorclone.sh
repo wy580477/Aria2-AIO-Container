996fcae013e515182ea3144522020a7a9276f96d
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-04-26 11:37:02 +0800

    Update ytdlptorclone.sh

--- content/ytdlptorclone.sh
+++ content/ytdlptorclone.sh
@@ -12,7 +12,7 @@ FILEPATH=$(echo $1 | sed 's:[^/]*$::')
 FILENAME=$(echo $1 | sed 's:/.*/::')
 
 if [[ "${POST_MODE}" =~ "move" ]]; then
-    curl -s -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"srcFs":"'"${FILEPATH}"'","srcRemote":"'"${FILENAME}"'","dstFs":"'"${DRIVENAME}"':/'"${DRIVE_DIR}"'","dstRemote":"'"${FILENAME}"'","_async":"true"}' ''${RCLONE_ADDR}'/operations/movefile'
+    curl -s -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"srcFs":"'"${FILEPATH}"'","srcRemote":"'"${FILENAME}"'","dstFs":"'"${DRIVENAME}"':'"${DRIVE_DIR}"'","dstRemote":"'"${FILENAME}"'","_async":"true"}' ''${RCLONE_ADDR}'/operations/movefile'
 elif [[ "${POST_MODE}" =~ "copy" ]]; then
-    curl -s -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"srcFs":"'"${FILEPATH}"'","srcRemote":"'"${FILENAME}"'","dstFs":"'"${DRIVENAME}"':/'"${DRIVE_DIR}"'","dstRemote":"'"${FILENAME}"'","_async":"true"}' ''${RCLONE_ADDR}'/operations/copyfile'
+    curl -s -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"srcFs":"'"${FILEPATH}"'","srcRemote":"'"${FILENAME}"'","dstFs":"'"${DRIVENAME}"':'"${DRIVE_DIR}"'","dstRemote":"'"${FILENAME}"'","_async":"true"}' ''${RCLONE_ADDR}'/operations/copyfile'
 fi
