22ec8d4399ae6e465e8eb9f48060789fd7087f58
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-05-02 18:24:47 +0800

    minor bug fixes

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -5,10 +5,10 @@ if [ "${PUID}" = "0" ]; then
     ln -s /.aria2allinoneworkdir/service/* /etc/service/ 2>/dev/null
     exec runsvdir -P /etc/service
 else
-    mkdir -p /mnt/data/downloads /mnt/data/finished 2>/dev/null
+    mkdir -p /mnt/data/downloads /mnt/data/finished /mnt/data/videos 2>/dev/null
     rm -rf /etc/service/*
     chown -R ${PUID}:${PGID} /.aria2allinoneworkdir /mnt/config 2>/dev/null
-    chown ${PUID}:${PGID} /mnt/data /mnt/data/downloads /mnt/data/finished /etc/service
+    chown ${PUID}:${PGID} /mnt/data /mnt/data/downloads /mnt/data/finished /mnt/data/videos /etc/service
     su-exec ${PUID}:${PGID} ln -s /.aria2allinoneworkdir/service/* /etc/service/ 2>dev/null
     exec su-exec ${PUID}:${PGID} runsvdir -P /etc/service
 fi
--- content/ytdlptorclone.sh
+++ content/ytdlptorclone.sh
@@ -11,6 +11,8 @@ FILEPATH=$(echo $1 | sed 's:[^/]*$::')
 FILENAME=$(basename "$1")
 mv "$1" "${FILEPATH}""${FILENAME}"
 
+if [ "${POST_MODE}" = "move" ]; then
+    :
 if [[ "${POST_MODE}" =~ "move" ]]; then
     curl -s -S -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"srcFs":"'"${FILEPATH}"'","srcRemote":"'"${FILENAME}"'","dstFs":"'"${REMOTE_PATH}"'","dstRemote":"'"${FILENAME}"'","_async":"true"}' ''${RCLONE_ADDR}'/operations/movefile'
     EXIT_CODE=$?
