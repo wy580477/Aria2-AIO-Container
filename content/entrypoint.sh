6c8a35241f2c80baa1e62e768cac39f11b4cfc1f
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-05-05 09:55:18 +0800

    cleanup code

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 if [ "${PUID}" = "0" ]; then
-    chown -R ${PUID} /etc/service
     ln -s /.aria2allinoneworkdir/service/* /etc/service/ 2>/dev/null
     exec runsvdir -P /etc/service
 else
     mkdir -p /mnt/data/downloads /mnt/data/finished /mnt/data/videos 2>/dev/null
-    rm -rf /etc/service/*
     chown -R ${PUID}:${PGID} /.aria2allinoneworkdir /mnt/config 2>/dev/null
     chown ${PUID}:${PGID} /mnt/data /mnt/data/downloads /mnt/data/finished /mnt/data/videos /etc/service
     su-exec ${PUID}:${PGID} ln -s /.aria2allinoneworkdir/service/* /etc/service/ 2>dev/null
